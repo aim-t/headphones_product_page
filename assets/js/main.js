91ae80ba2e0ad39134004be8a5979188052390ff
aim-t <namiatariq@gmail.com>
2023-05-18 20:31:35 +0500

    Finish project

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,11 +3,16 @@ const navMenu = document.getElementById('nav-menu');
 const navToggle = document.getElementById('nav-toggle');
 const navClose = document.getElementById('nav-close');
 
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
 if(navToggle){
     navToggle.addEventListener('click', ()=>{
         navMenu.classList.add('show-menu');
     })
 }
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if(navClose){
     navClose.addEventListener('click', ()=>{
         navMenu.classList.remove('show-menu');
@@ -43,7 +48,7 @@ window.addEventListener('scroll', scrollUp);
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive(){
-    const scrollY = window.scrollY;
+    const scrollY = window.pageYOffset;
 
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight;
@@ -51,14 +56,29 @@ function scrollActive(){
         sectionId =  current.getAttribute('id');
     
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
-        } else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
-    });
+    })
 }
 window.addEventListener('scroll', scrollActive);
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+});
 
+sr.reveal(`.home__header, .section__title`, {delay: 600});
+sr.reveal(`.home__footer`, {delay: 700});
+sr.reveal(`.home__img`, {delay: 900, origin: 'top'});
+
+sr.reveal(`.sponsor__img, .products__card, .footer__logo, .footer__content, .footer__copy`, {origin: 'top', interval: 100});
+sr.reveal(`.specs__data, .discount__animate`, {origin: 'left', interval: 100});
+sr.reveal(`.specs__img, .discount__img`, {origin: 'right'});
+sr.reveal(`.case__img`, {origin: 'top'});
+sr.reveal(`case__data`);
